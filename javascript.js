938a3c88dc925e62de58da3a222f66a7715bc0a2
Alex <alexandruivan1995@gmail.com>
2021-03-10 20:43:47 -0600

    love

--- javascript.js
+++ javascript.js
@@ -191,3 +191,23 @@ function draw() {
                 });
 
             }
+
+// Love
+let start = Date.now();
+
+let timer = setInterval(function() {
+  
+  let timePassed = Date.now() - start;
+
+  if (timePassed >= 2000) {
+    clearInterval(timer); 
+    return;
+  }
+
+  draw(timePassed);
+
+}, 20);
+
+function draw(timePassed) {
+  heart.style.left = timePassed / 5 + 'px';
+}
